f91a6d7c93e06a4cbfbed03a0987d9328027f98d
Alexander Schimanko <as323@hdm-stuttgart.de>
2023-12-05 12:13:59 +0100

    added different ball types

--- ball.gd
+++ ball.gd
@@ -1,14 +1,35 @@
 extends RigidBody2D
 
-@export var MAX_SPEED_y = -1000.0
+signal hit_left_floor
+signal hit_right_floor
+
+enum BALL {DEFAULT, SLOW, BIG, BOUNCE}
+
+@export var ball_type = BALL.SLOW
+
 @export var MAX_SPEED_x = 600.0
-@export var MIN_SPEED_y = -500.0
+@export var MAX_SPEED_y = -1000.0
 @export var MIN_SPEED_x = 200.0
+@export var MIN_SPEED_y = -500.0
 
 
 var gravity = 1000
 func _ready():
-	pass # Replace with function body.
+	match ball_type:
+		BALL.SLOW:
+			self.gravity_scale = 0.2
+			MAX_SPEED_x = MAX_SPEED_x/2
+			MAX_SPEED_y = MAX_SPEED_y/2
+			MIN_SPEED_x = MIN_SPEED_x/3
+			MIN_SPEED_y = MIN_SPEED_y/3
+		BALL.BIG:
+			$Sprite2D.scale = Vector2(1.5,1.5)
+			$CollisionShape2D.shape.radius = 48
+		BALL.BOUNCE:
+			MAX_SPEED_x = MAX_SPEED_x*5
+			MAX_SPEED_y = MAX_SPEED_y*5
+			MIN_SPEED_x = MIN_SPEED_x*5
+			MIN_SPEED_y = MIN_SPEED_y*5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -52,14 +73,18 @@ func _on_body_entered(body):
 		if abs(linear_velocity.x) > MAX_SPEED_x:
 			linear_velocity.x = linear_velocity.x * 1.2
 			if abs(linear_velocity.x) > MAX_SPEED_x:
-				linear_velocity.x = MAX_SPEED_x
+				linear_velocity.x = (linear_velocity.x/abs(linear_velocity.x))*MAX_SPEED_x
 		if abs(linear_velocity.x) > MAX_SPEED_x:
-			linear_velocity.x = MAX_SPEED_x
+			print(linear_velocity.x)
+			linear_velocity.x = (linear_velocity.x/abs(linear_velocity.x))*MAX_SPEED_x
 		if linear_velocity.y < MAX_SPEED_y:
 			linear_velocity.y = linear_velocity.y * 1.2
 			if linear_velocity.y < MAX_SPEED_y:
 				linear_velocity.y = MAX_SPEED_y
 		if linear_velocity.y > MIN_SPEED_y:
 			linear_velocity.y = MIN_SPEED_y
-		print(linear_velocity)
+	if body.is_in_group("left_floor"):
+		hit_left_floor.emit()
+	if body.is_in_group("right_floor"):
+		hit_right_floor.emit()
 
